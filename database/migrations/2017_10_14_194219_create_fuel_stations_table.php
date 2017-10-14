0556f7bc9dd28460a60e173dec83ba9a0f89c7f4
Roberto Arruda <robertoadearruda@gmail.com>
2017-10-14 19:08:43 -0300

    Migrate

---
+++ database/migrations/2017_10_14_194219_create_fuel_stations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFuelStationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fuel_stations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100)->index();
+            $table->string('cnjp', 14)->unique();
+            $table->string('address', 14);
+            $table->string('phone', 11);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fuel_stations');
+    }
+}
