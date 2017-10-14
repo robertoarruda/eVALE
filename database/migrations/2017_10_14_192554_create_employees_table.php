284216621d0b28673317db8735490b4e1feb5748
eVALE <evalesoftware@gmail.com>
2017-10-14 19:08:43 -0300

    Migrate

---
+++ database/migrations/2017_10_14_192554_create_employees_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('company_id')->unsigned();
+            $table->string('registration_number')->unique();
+            $table->string('name', 100)->index();
+            $table->string('password', 8);
+            $table->decimal('balance', 5, 2);
+            $table->timestamps();
+
+            $table->foreign('company_id')
+                ->references('id')->on('companies')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
