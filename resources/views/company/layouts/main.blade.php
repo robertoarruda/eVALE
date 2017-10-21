db87e69f7b1b12ba5a6e5e4fe433af37e1515834
Roberto Arruda <robertoadearruda@gmail.com>
2017-10-21 18:34:13 -0200

    Ajustes layout

--- resources/views/company/layouts/main.blade.php
+++ resources/views/company/layouts/main.blade.php
@@ -11,20 +11,18 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{ route('company.index') }}">{{ config('app.name') }}</a>
+                <a class="navbar-brand" href="{{ route('company.index') }}">
+                    <b>{{ config('app.name') }}</b>
+                </a>
             </div>
             <!-- /.navbar-header -->
 
             <ul class="nav navbar-top-links navbar-right">
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                        <i class="fa fa-user fa-fw"></i> {{ ucwords(Auth::user()->name) }} <i class="fa fa-caret-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-user">
-                        <li>
-                            <a href="javascript:void(0)"><i class="fa fa-user fa-fw"></i> {{ ucwords(Auth::user()->name) }}</a>
-                        </li>
-                        <li class="divider"></li>
                         <li>
                             <a href="{{ route('company.logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                                 <i class="fa fa-sign-out fa-fw"></i> Logout
