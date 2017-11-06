298e278790e67bf96e59bd7b700ad7055a8a31d4
Roberto Arruda <robertoadearruda@gmail.com>
2017-11-05 23:34:18 -0200

    Ajustes

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd /var/www/html/eVALE
 
 git init && git remote add origin git@gitlab.com:robertoadearruda/eVALE.git && git fetch origin && git checkout -f master && git pull origin master
 
-chmod -R 777 storage && chmod -R 777 bootstrap/cache
+chown -R 1000:1000 ../eVALE && chmod -R 777 storage && chmod -R 777 bootstrap/cache
 git checkout -- . && rm -rf .git
 
 composer install
